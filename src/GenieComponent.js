52eceb65edbf64281eedee9cbe157a0573e85512
usman <usmanasgh@gmail.com>
2020-11-12 15:26:51 +0000

    adding new component geine

---
+++ src/GenieComponent.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function GenieComponent() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>Testing Genie Component</p>
+        <p>Pass Genie coponent from 'index.js' to see the results.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default GenieComponent;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Genie from './GenieComponent';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  document.getElementById('root')
+); */
+
+// MUA: Calling one 'APP' Component.
+ReactDOM.render(
+  <Genie/>,
   document.getElementById('root')
 );
 
